2baa65b25bd5a83e3a16b02124bafb667b61ccfa
Mathis Boulais <77585805+Asukyy@users.noreply.github.com>
2022-07-20 19:00:45 +0200

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,11 @@
+from flask import request, Flask 
+import json
+
+app1=Flask(value)
+@app1.route('/')
+
+def index():
+    return 'Hello, World!'
+
+if value == 'app1':
+    app1.run(debug=True, host='0.0.0.0')
